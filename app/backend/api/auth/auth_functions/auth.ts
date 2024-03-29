5bcf2a60b0e421d4fb379014d24f6da59d9bf0ae
Siddharth Kumar <99739245+Sid435@users.noreply.github.com>
2024-03-30 01:49:12 +0530

    caterpie

--- app/backend/api/auth/auth_functions/auth.ts
+++ app/backend/api/auth/auth_functions/auth.ts
@@ -5,17 +5,17 @@ import {Prisma} from '@prisma/client'
 type OTP = {
     id : string,
     otp : string,
-    phone_number : string
+    email : string
 }
 
 export async function auth(req : Request){
     const jsonData = await req.json();
 
-    const phone_number = jsonData.email;
+    const email = jsonData.email;
     const otp = jsonData.pass;
 
-    if(await checkUser(phone_number)){
-        const generateOtp = await generate_otp_and_send(phone_number);
+    if(await checkUser(email)){
+        const generateOtp = await generate_otp_and_send(email);
         return NextResponse.json({token : `${generateOtp}`})
     }else {
         return NextResponse.json({message : "Unauthorized"}, {status : 403})
@@ -26,8 +26,7 @@ export async function addUser(req : Request){
     const jsonData = await req.json();
     const userData = {
         name :  jsonData.name,
-        email : jsonData.email,
-        phone_number : jsonData.phone_number
+        email : jsonData.email
     } as Prisma.UserCreateInput;
     try{ 
         await prisma?.user.create({
@@ -40,53 +39,53 @@ export async function addUser(req : Request){
 
 }
 
-async function checkUser(phone_number : string){
+async function checkUser(email : string){
     const user = await prisma?.user.findMany({
-        where : {phone_number : phone_number}
+        where : {email : email}
     })
     return user!!;
 }
 
-async function generateToken(phone_number : string){
+async function generateToken(email : string){
     const payload = {
-        phone_number : phone_number
+        email : email
     }
     const token = jwt.sign(payload, process.env.JWT_SECRET_KEY!, {expiresIn : "60m"});
 
     return token;
 }
 
-async function generate_otp_and_send(phone_number: string) {
+async function generate_otp_and_send(email: string) {
     const randomNum = Math.floor(Math.random() * (999999 + 1)) + 0;
     const randOtp = randomNum.toString().padStart(6, "0"); // Six digit OTP
     const payload = {
         otp : randOtp,
-        phone_number:phone_number
+        email:email
     }
     const message = `Your OTP verification code : ${randOtp}`;
-    await sendSMS(message, phone_number);
+    await sendEmail(message, email);
     const otpToken = jwt.sign(payload, process.env.OTP_SECRET_KEY!, {expiresIn : "5m"});
     await prisma?.user.update({
-        where :{phone_number : phone_number},
+        where :{email : email},
         data : {otp : otpToken}
     })
     return otpToken;
 }
-async function sendSMS(message: string, phone_number: String) {
+async function sendEmail(message: string, phone_number: String) {
     throw new Error("Function not implemented."); //send OTP Using Twilio
 }
 
 export async function verifyOtp(req : Request){
     const jsonData = await req.json();
-    const phone_number = jsonData.phone_number;
+    const email = jsonData.email;
     try{
         const otpFromData = await prisma?.user.findFirst({
-             where : {phone_number: phone_number}
+             where : {email: email}
         })
         const decode  = jwt.verify(otpFromData?.otp!, process.env.OTP_SECRET_KEY!) as OTP;
 
         if(decode.otp === jsonData.otp){
-            return generateToken(phone_number);
+            return generateToken(email);
         }else {
             return NextResponse.json({message : "Invlid"},{status : 403})
         }
